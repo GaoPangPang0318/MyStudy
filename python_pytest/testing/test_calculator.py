e1fb4e5b6950154b1d073c9cefe9e0ec1d657d80
GaoPangPang0318 <719453296@qq.com>
2021-02-01 19:28:24 +0800

    python-pytest_fixture 课后作业

--- python_pytest/testing/test_calculator.py
+++ python_pytest/testing/test_calculator.py
@@ -6,61 +6,68 @@
     4.控制测试用例顺序按照【加-减-乘-除】这个顺序执行
     5.结合 allure 生成本地测试报告
 """
-
+import allure
 import pytest
-import yaml
 
 
-def get_data(dataflag:str)->list:
-    """
-        获取yaml中的数据
-    :param dataflag: 获取测试数据类型标志  add：加法测试数据 sub:减法测试数据 mul：乘法测试数据 div：除法测试数据
-    :return:  测试数据和标记的list
-    """
-    with open("../datas/cal_datas.yml") as f:
-        data=yaml.safe_load(f)[dataflag]
-        testing_data=data['datas']  #测试数据
-        ids=data['ids']  #ids
-        return [testing_data,ids]
+# 禁用原因：每调用一次都会open一次文件然后再关闭，当测试用例增多时既影响效率又占用资源，因此不是一个好方法
+#
+# def get_data(dataflag:str)->list:
+#     """
+#         获取yaml中的数据
+#     :param dataflag: 获取测试数据类型标志  add：加法测试数据 sub:减法测试数据 mul：乘法测试数据 div：除法测试数据
+#     :return:  测试数据和标记的list
+#     """
+#     with open("../datas/cal_datas.yml") as f:
+#         data=yaml.safe_load(f)[dataflag]
+#         testing_data=data['datas']  #测试数据
+#         ids=data['ids']  #ids
+#         return [testing_data,ids]
 
-
-
-
+@allure.feature("测试计算器")
 class TestCal:
-    #加法测试用例，并进行参数化
+    # 加法测试用例,并设置执行顺序为1
+    @allure.story("加法测试")
     @pytest.mark.run(order=1)
-    @pytest.mark.parametrize("a,b,expect",get_data("add")[0],ids=get_data("add")[1])
-    def test_add(self,a,b, expect,get_calc):
-        result=get_calc.add(a,b)
-        if isinstance(result,float):
-             result=round(result,2)
-        assert result==expect
+    # @pytest.mark.parametrize("a,b,expect",get_data("add")[0],ids=get_data("add")[1])
+    def test_add(self, get_calc, get_datas_add):
+        with allure.step("计算两数之和："):
+            result = get_calc.add(get_datas_add[0], get_datas_add[1])
 
-    #除法测试用例
+        # 对浮点数测试用例，结果精确到小数点后两位
+        if isinstance(result, float):
+            result = round(result, 2)
+        assert result == get_datas_add[2]
+
+    # 除法测试用例，并设置执行顺序为4
+    @allure.story("除法测试")
     @pytest.mark.run(order=4)
-    @pytest.mark.parametrize("a,b,expect", get_data("div")[0], ids=get_data("div")[1])
-    def test_div(self,a,b, expect,get_calc):
-        result = get_calc.div(a, b)
+    # @pytest.mark.parametrize("a,b,expect", get_data("div")[0], ids=get_data("div")[1])
+    def test_div(self, get_calc, get_datas_div):
+        with allure.step("计算两数之商："):
+            result = get_calc.div(get_datas_div[0], get_datas_div[1])
         if isinstance(result, float):
             result = round(result, 2)
-        assert result == expect
+        assert result == get_datas_div[2]
 
-
-    #减法测试用例
+    # 减法测试用例，并设置执行顺序为2
+    @allure.story("减法测试")
     @pytest.mark.run(order=2)
-    @pytest.mark.parametrize("a,b,expect", get_data("sub")[0], ids=get_data("sub")[1])
-    def test_sub(self,a,b, expect,get_calc):
-        result = get_calc.sub(a, b)
+    # @pytest.mark.parametrize("a,b,expect", get_data("sub")[0], ids=get_data("sub")[1])
+    def test_sub(self, get_calc, get_datas_sub):
+        with allure.step("计算两数之差："):
+            result = get_calc.sub(get_datas_sub[0], get_datas_sub[1])
         if isinstance(result, float):
             result = round(result, 2)
-        assert result == expect
+        assert result == get_datas_sub[2]
 
-    #乘法测试用例
+    # 乘法测试用例，并设置执行顺序为3
+    @allure.story("乘法测试")
     @pytest.mark.run(order=3)
-    @pytest.mark.parametrize("a,b,expect", get_data("mul")[0], ids=get_data("mul")[1])
-    def test_mul(self,a,b, expect,get_calc):
-        result = get_calc.mul(a, b)
+    # @pytest.mark.parametrize("a,b,expect", get_data("mul")[0], ids=get_data("mul")[1])
+    def test_mul(self, get_calc, get_datas_mul):
+        with allure.step("计算两数之积："):
+            result = get_calc.mul(get_datas_mul[0], get_datas_mul[1])
         if isinstance(result, float):
             result = round(result, 2)
-        assert result == expect
-
+        assert result == get_datas_mul[2]
