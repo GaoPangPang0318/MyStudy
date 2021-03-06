c56c1a8314b07df0ba30774c798a5a3e5b2992f6
GaoPangPang0318 <719453296@qq.com>
2021-03-06 22:51:19 +0800

    python_selenium_po:添加获取成员名字用于断言

--- python_selenium_po/page/base_page.py
+++ python_selenium_po/page/base_page.py
@@ -4,9 +4,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
-
 
 class BasePage:
 
