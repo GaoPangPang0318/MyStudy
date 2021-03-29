b44573f8db58e8eadde4f37df324e8408c2f7167
GaoPangPang0318 <719453296@qq.com>
2021-03-29 16:59:39 +0800

    修改

--- python_requests/testcase/test_contacts.py
+++ python_requests/testcase/test_contacts.py
@@ -36,27 +36,27 @@ class TestContacts(TestCaseBase):
         :param department: 创建用户需要信息department
         :param otherinfo: 创建用户其他信息
         """
-        # 是否存在userid
-        find_result = self.contacts.get_member(userid)
-        # 存在则删除
-        if find_result['errmsg'] == 'ok':
-            print(f"UserID={userid} is exited.")
+        try:
+            r = self.contacts.create_member(userid=userid, name=name, mobile=mobile, department=department, **otherinfo)
+        finally:
             delete_result = self.contacts.delete_member(userid)
-            if delete_result['errmsg'] == 'deleted':
-                print(f"Successful Delete {userid}.")
-        # 创建成员
-        r = self.contacts.create_member(userid=userid, name=name, mobile=mobile, department=department, **otherinfo)
         assert r['errmsg'] == 'created'
+        assert delete_result['errcode']==0
 
-    @pytest.mark.parametrize("userid,result", TestCaseBase.get_testdata('getmember'))
-    def test_getmember(self, userid, result: int):
-        """
-        获取用户信息测试用例
-        :param userid:
-        :param result: 预期返回错误码
-        """
-        r = self.contacts.get_member(userid=userid)
-        assert r['errcode'] == result
+    # @pytest.mark.parametrize("userid,result", TestCaseBase.get_testdata('getmember'))
+    # def test_getmember(self, userid, result: int):
+    #     """
+    #     获取用户信息测试用例
+    #     :param userid:
+    #     :param result: 预期返回错误码
+    #     """
+    #     self.contacts.create_member('lisi','李四','18320202022',[1,2])
+    #     try:
+    #         r = self.contacts.get_member(userid=userid)
+    #     finally:
+    #         self.contacts.delete_member(userid)
+    #     assert r['errcode'] == result
+
 
     @pytest.mark.parametrize("userid,changeinfo", TestCaseBase.get_testdata('updatemember'))
     def test_updatemember(self, userid, changeinfo: Dict):
@@ -65,7 +65,11 @@ class TestContacts(TestCaseBase):
         :param userid:
         :param changeinfo: 要修改的参数
         """
-        r = self.contacts.update_member(userid=userid, **changeinfo)
+        self.contacts.create_member('zhangsan','王二','18320202023',[1,3],gender=2,position='研发经理')
+        try:
+            r = self.contacts.update_member(userid=userid, **changeinfo)
+        finally:
+            self.contacts.delete_member(userid)
         assert r['errcode'] == 0
 
     @pytest.mark.parametrize("userid,result", TestCaseBase.get_testdata('deletemember'))
@@ -75,6 +79,7 @@ class TestContacts(TestCaseBase):
         :param userid:
         :param result: 预期返回的errcode
         """
+        self.contacts.create_member('lisi', '李四', '18320202022', [1, 2])
         r = self.contacts.delete_member(userid=userid)
         assert r['errcode'] == result
 #
